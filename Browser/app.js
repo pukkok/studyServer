d1e2e9d3656bb40e4c83ab185dda95a2be876751
Pukkok <krystalgggg@gmail.com>
2024-05-08 00:27:40 +0900

    브라우저 연결 관리해보기

--- Browser/app.js
+++ Browser/app.js
@@ -1,2 +1,33 @@
 const BASE_URL = 'http://localhost:4000'
-console.log(BASE_URL)
+
+const login = async (id, pw) => {
+    const userJSON = await fetch(`${BASE_URL}/api/users/login`, {
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        method : 'POST',
+        body : JSON.stringify({
+            id, pw
+        })
+    })
+    console.log(userJSON.json().token)
+    return userJSON.json()
+}
+
+const idInput = document.getElementById('id')
+const pwInput = document.getElementById('pw')
+const loginBtn = document.querySelector('.login-btn')
+
+const register = document.querySelector('.register')
+
+loginBtn.addEventListener('click', ()=>{
+    login(idInput.value, pwInput.value)
+})
+
+register.addEventListener('click', ()=> {
+    
+})
+
+// const getGroup = async (field, token) => {
+//     const groupJSON = await fetch(`${BASE_URL}/api/`)
+// }
